13fd97f4122db14a91d89b86685b8debeefa2783
starbelly@pobox.com <starbelly@pobox.com>
2018-06-25 22:33:47 -0500

    YAGNI on the the provider for the moment

--- src/rebar3_local.erl
+++ src/rebar3_local.erl
@@ -3,5 +3,5 @@
 -export([init/1]).
 
 -spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
-init(State) ->
-     {ok, rebar_state:add_resource(State, {local, rebar3_local_resource})}.
+init(State) -> 
+  {ok, rebar_state:add_resource(State, {local, rebar3_local_resource})}.
