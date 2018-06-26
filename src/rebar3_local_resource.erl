898917ff609d1fbc68b13e784bb0f63a6879aa6a
starbelly@pobox.com <starbelly@pobox.com>
2018-06-25 22:23:44 -0500

    get abs path via rebar_state:dir/1

--- src/rebar3_local_resource.erl
+++ src/rebar3_local_resource.erl
@@ -12,18 +12,17 @@ needs_update(_AppDir, _Resource) -> true.
 
 download(AppDir, {local, Path}, _State) ->
   download(AppDir, {local, Path, {exclude, []}}, _State);
-download(AppDir, {local, Path, {exclude, Exclusions}}, _State) ->
-  {ok, Cwd} = file:get_cwd(),
-  Files = valid_file_list(filename:join([Cwd, "local", Path]), Exclusions),
+download(AppDir, {local, Path, {exclude, Exclusions}}, State) ->
+  Files = dep_files(filename:join([rebar_state:dir(State), "local", Path]), Exclusions),
   {rebar_file_utils:cp_r(Files, AppDir), undefined}.
 
-valid_file_list(FromDir, Exclusions) ->
-  {ok, Files} = file:list_dir(FromDir),
-  lists:filtermap(fun (F) -> valid_file(FromDir, F, Exclusions) end, Files).
+dep_files(FromDir, Exclusions) ->
+  {ok, List} = file:list_dir(FromDir),
+  lists:filtermap(fun (F) -> file_filter_join(FromDir, F, Exclusions) end, List).
 
-valid_file(FromDir, File, Exclusions) ->
+file_filter_join(FromDir, File, Exclusions) ->
   case lists:member(File, Exclusions ++ ?DEFAULT_EXCLUDED_FILES) of
-	true -> false; 
+	true -> false;
     false -> {true, filename:join([FromDir, File])}
   end.
 
